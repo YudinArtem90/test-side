e7833981eee5ca160f96a33efda539abb7e7b9cf
YudinArtem90 <yudin.artem.90@yandex.ru>
2025-07-20 13:36:45 +0300

    fix class header + menu

---
+++ js/menu.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.site-header__burger');
+  const navIconsWrapper = document.querySelector('.site-header__nav-icons');
+  const separator = document.querySelector('.site-header__separator');
+
+  const toggleMenu = () => {
+    navIconsWrapper.classList.toggle('active');
+    burger.classList.toggle('is-active');
+    document.body.classList.toggle('no-scroll');
+    separator.classList.toggle('site-header__separator--visible');
+  };
+
+  burger.addEventListener('click', (e) => {
+    e.stopPropagation();
+    toggleMenu();
+  });
+
+  document.addEventListener('click', (e) => {
+    const isClickInside = navIconsWrapper.contains(e.target);
+    if (!isClickInside && navIconsWrapper.classList.contains('active')) {
+      toggleMenu();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && navIconsWrapper.classList.contains('active')) {
+      toggleMenu();
+    }
+  });
+});
