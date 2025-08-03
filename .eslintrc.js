d5004a88b96353a0b95c8b66035ebbd6a3d27709
YudinArtem90 <yudin.artem.90@yandex.ru>
2025-08-03 15:15:59 +0300

    add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['react'],
+  rules: {
+    // Примеры правил
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+  },
+};
