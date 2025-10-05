043a7597797a1d74d3f94233ac1010766a3e1253
YudinArtem90 <yudin.artem.90@yandex.ru>
2025-10-05 09:53:53 +0300

    fix carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,19 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   const list = document.querySelector('.reviews__list');
 
-  // инициализация только если <= 1024
   function initCarousel() {
-    if (
-      window.innerWidth <= 1024 &&
-      !list.classList.contains('flickity-enabled')
-    ) {
+    const reviewsImage = document.getElementsByClassName('reviews__image');
+    const isFlickityEnabledActive = list.classList.contains('flickity-enabled');
+    if (window.innerWidth <= 1522 && !isFlickityEnabledActive) {
+      reviewsImage[0].remove();
       new Flickity(list, {
         cellAlign: 'left',
         contain: true,
         pageDots: false,
         prevNextButtons: true,
         wrapAround: true,
+        draggable: true,
+        touchVerticalScroll: true,
       });
+    } else if (!reviewsImage.length && !isFlickityEnabledActive) {
+      const div = document.createElement('div');
+      div.className = 'reviews__image';
     }
   }
 
