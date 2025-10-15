c953b2375ef81bcfaade7ceb448c58250373a655
YudinArtem90 <yudin.artem.90@yandex.ru>
2025-10-15 09:59:14 +0300

    add carousel services__grid in 344px

--- js/carousel.js
+++ js/carousel.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
   function initCarousel() {
     const reviewsImage = document.getElementsByClassName('reviews__image');
     const isFlickityEnabledActive = list.classList.contains('flickity-enabled');
+    const servicesGrid = document.querySelector('.services__grid');
+
     if (window.innerWidth <= 1522 && !isFlickityEnabledActive) {
       reviewsImage[0].remove();
       new Flickity(list, {
@@ -18,6 +20,19 @@ document.addEventListener('DOMContentLoaded', function () {
     } else if (!reviewsImage.length && !isFlickityEnabledActive) {
       const div = document.createElement('div');
       div.className = 'reviews__image';
+    }
+
+    if (window.innerWidth <= 344) {
+      new Flickity(servicesGrid, {
+        cellAlign: 'left',
+        contain: true,
+        pageDots: false,
+        prevNextButtons: false,
+        wrapAround: true,
+        draggable: true,
+        touchVerticalScroll: true,
+        groupCells: 1,
+      });
     }
   }
 
