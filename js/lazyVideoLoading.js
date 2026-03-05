12667a8e55f94d94186c98408d4e401a3bbf6b03
YudinArtem90 <yudin.artem.90@yandex.ru>
2026-03-05 20:26:59 +0300

    replaced the video and unified the video block

--- js/lazyVideoLoading.js
+++ js/lazyVideoLoading.js
@@ -1,33 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const videoBlock = document.querySelector('.about__media--video');
-  const playBtn = videoBlock.querySelector('.about__media-play');
-  const mediaInner = videoBlock.querySelector('.about__media-inner--video');
-  const videoSrc = videoBlock.getAttribute('data-video');
+  const videos = document.querySelectorAll('.video');
 
-  playBtn.addEventListener('click', () => {
-    // создаём контейнер для iframe
-    const iframeWrapper = document.createElement('div');
-    iframeWrapper.className = 'about__media-video';
+  videos.forEach((video) => {
+    const playBtn = video.querySelector('.video__play');
+    const inner = video.querySelector('.video__inner');
+    const thumb = video.querySelector('.video__thumb');
+    const videoSrc = video.dataset.video;
 
-    const iframe = document.createElement('iframe');
-    iframe.src = videoSrc + '?autoplay=1';
-    iframe.allow =
-      'autoplay; fullscreen; picture-in-picture; encrypted-media; gyroscope; accelerometer; clipboard-write; screen-wake-lock;';
-    iframe.allowFullscreen = true;
+    if (!playBtn || !videoSrc) return;
 
-    iframeWrapper.appendChild(iframe);
-    mediaInner.appendChild(iframeWrapper);
+    playBtn.addEventListener('click', () => {
+      const iframeWrapper = document.createElement('div');
+      iframeWrapper.className = 'video__iframe';
 
-    // плавно убираем картинку и кнопку
-    const img = mediaInner.querySelector('img');
-    img.style.opacity = '0';
-    playBtn.style.opacity = '0';
+      const iframe = document.createElement('iframe');
+      iframe.src = videoSrc + '?autoplay=1';
+      iframe.allow = 'autoplay; fullscreen; picture-in-picture;';
+      iframe.allowFullscreen = true;
 
-    // немного подождать, затем показываем видео
-    setTimeout(() => {
-      iframeWrapper.classList.add('active');
-      img.remove();
-      playBtn.remove();
-    }, 200);
+      iframeWrapper.appendChild(iframe);
+      inner.appendChild(iframeWrapper);
+
+      // плавно скрываем картинку и кнопку
+      if (thumb) thumb.style.opacity = '0';
+      playBtn.style.opacity = '0';
+
+      // удаляем после анимации
+      setTimeout(() => {
+        if (thumb) thumb.remove();
+        playBtn.remove();
+      }, 300);
+    });
   });
 });
