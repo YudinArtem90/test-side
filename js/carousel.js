468ef9b2b67dc30bbdcb4a3003d616c7a721b142
YudinArtem90 <yudin.artem.90@yandex.ru>
2026-03-18 17:48:39 +0300

    added a general carousel and redesigned the accounting-remotely and reviews section

--- js/carousel.js
+++ js/carousel.js
@@ -1,41 +1,63 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const list = document.querySelector('.reviews__list');
+document.addEventListener('DOMContentLoaded', () => {
+  const carousels = document.querySelectorAll('.carousel');
 
-  function initCarousel() {
-    const reviewsImage = document.getElementsByClassName('reviews__image');
-    const isFlickityEnabledActive = list.classList.contains('flickity-enabled');
-    const servicesGrid = document.querySelector('.services__grid');
+  carousels.forEach((carousel) => {
+    const track = carousel.querySelector('.carousel__track');
+    if (!track) return;
 
-    if (window.innerWidth <= 1522 && !isFlickityEnabledActive) {
-      reviewsImage[0].remove();
-      new Flickity(list, {
-        cellAlign: 'left',
-        contain: true,
-        pageDots: false,
-        prevNextButtons: true,
-        wrapAround: true,
-        draggable: true,
-        touchVerticalScroll: true,
-      });
-    } else if (!reviewsImage.length && !isFlickityEnabledActive) {
-      const div = document.createElement('div');
-      div.className = 'reviews__image';
+    const isReviews = carousel.classList.contains('carousel--reviews');
+
+    let flkty = null;
+
+    function initCarousel() {
+      const isFlickityEnabledActive =
+        track.classList.contains('flickity-enabled');
+
+      if (isReviews) {
+        const reviewsImage = carousel.querySelectorAll('.reviews__image');
+
+        if (window.innerWidth <= 1522 && reviewsImage.length) {
+          reviewsImage[0].remove();
+        }
+
+        if (window.innerWidth <= 1522 && !isFlickityEnabledActive) {
+          flkty = new Flickity(track, {
+            cellSelector: '.carousel__slide',
+            cellAlign: 'left',
+            contain: true,
+            pageDots: false,
+            prevNextButtons: window.innerWidth > 344,
+            wrapAround: true,
+            draggable: true,
+            groupCells: 1,
+          });
+        }
+
+        if (window.innerWidth > 1522 && isFlickityEnabledActive) {
+          flkty.destroy();
+          flkty = null;
+        }
+
+        return;
+      }
+
+      if (!isFlickityEnabledActive) {
+        flkty = new Flickity(track, {
+          cellSelector: '.carousel__slide',
+          cellAlign: 'center',
+          fullscreen: true,
+          lazyLoad: true,
+          contain: true,
+          pageDots: false,
+          prevNextButtons: true,
+          wrapAround: true,
+          draggable: true,
+        });
+      }
     }
 
-    if (window.innerWidth <= 344) {
-      new Flickity(servicesGrid, {
-        cellAlign: 'left',
-        contain: true,
-        pageDots: false,
-        prevNextButtons: false,
-        wrapAround: true,
-        draggable: true,
-        touchVerticalScroll: true,
-        groupCells: 1,
-      });
-    }
-  }
+    initCarousel();
 
-  initCarousel();
-  window.addEventListener('resize', initCarousel);
+    window.addEventListener('resize', initCarousel);
+  });
 });
