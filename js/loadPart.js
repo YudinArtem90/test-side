55b075e1432bf436085e7963e73b3366c9e59999
YudinArtem90 <yudin.artem.90@yandex.ru>
2026-03-22 16:07:51 +0300

    fix loadPart

--- js/loadPart.js
+++ js/loadPart.js
@@ -13,5 +13,5 @@ async function loadPart(id, file, callback) {
   }
 }
 
-loadPart('header', '../header.html', initMenu);
-loadPart('footer', '../footer.html');
+loadPart('header', '/header.html', initMenu);
+loadPart('footer', '/footer.html');
